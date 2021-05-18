8c424939a6aa740c9e35d1b7a13c03cf0b562d07
Dmitriy <dmitriycoupem@gmail.com>
2021-05-18 12:56:24 +0300

    Updates

---
+++ precache-manifest.824edc69b92f810260edc494d5f7c94b.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "005cdc5e694d5e472a9f8dac47fd599b",
+    "url": "/money-converter/index.html"
+  },
+  {
+    "revision": "3e78a0921460abf0bd8a",
+    "url": "/money-converter/static/css/2.b1176216.chunk.css"
+  },
+  {
+    "revision": "3e78a0921460abf0bd8a",
+    "url": "/money-converter/static/js/2.9a488472.chunk.js"
+  },
+  {
+    "revision": "465376c0c4b9728ff624339d576d7e27",
+    "url": "/money-converter/static/js/2.9a488472.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5336919558956973556d",
+    "url": "/money-converter/static/js/main.2283af80.chunk.js"
+  },
+  {
+    "revision": "c2b4755e3d4bc36905b1",
+    "url": "/money-converter/static/js/runtime-main.415cecc2.js"
+  },
+  {
+    "revision": "a91ad097d24828af724d4fee36a063ed",
+    "url": "/money-converter/static/media/roboto-all-400-normal.a91ad097.woff"
+  },
+  {
+    "revision": "8bb64952764a884d67019b3486296ab9",
+    "url": "/money-converter/static/media/roboto-cyrillic-400-normal.8bb64952.woff2"
+  },
+  {
+    "revision": "4743c758a952f2bd4a35d4e42afc002b",
+    "url": "/money-converter/static/media/roboto-cyrillic-ext-400-normal.4743c758.woff2"
+  },
+  {
+    "revision": "c1e9793c84cb26c44ef2a2cf8b6f49ce",
+    "url": "/money-converter/static/media/roboto-greek-400-normal.c1e9793c.woff2"
+  },
+  {
+    "revision": "182ee6a4872ca8fa78048951b1561a5c",
+    "url": "/money-converter/static/media/roboto-greek-ext-400-normal.182ee6a4.woff2"
+  },
+  {
+    "revision": "479970ffb74f2117317f9d24d9e317fe",
+    "url": "/money-converter/static/media/roboto-latin-400-normal.479970ff.woff2"
+  },
+  {
+    "revision": "455200cb007fe1212c668721d827c691",
+    "url": "/money-converter/static/media/roboto-latin-ext-400-normal.455200cb.woff2"
+  },
+  {
+    "revision": "a8be5b46d06bb541b0968196ee5e6bb8",
+    "url": "/money-converter/static/media/roboto-vietnamese-400-normal.a8be5b46.woff2"
+  }
+]);
